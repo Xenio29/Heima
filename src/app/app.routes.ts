bb5bbff45c83b6932118403508e28ff2fe38f7bc
Tigane Guelloul <93675913+Xenio29@users.noreply.github.com>
2026-06-07 18:22:32 +0200

    Routes + Custom Publish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Settings } from './components/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'settings',
+        component: Settings
+    },
+    {
+        path: '',
+        component: Dashboard
+    }
+];
